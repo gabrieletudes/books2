b620569a9f98b36021dfcb357df0c75b0c45b481
gabriel <gabriel@MacBook-Pro-van-gabriel.local>
2016-03-03 14:22:18 +0100

    First db access

--- index.php
+++ index.php
@@ -11,5 +11,32 @@
 /*Le constructeur est automatiquement executer quand on fait new
 
     Les parametres dsn (addrese base de donne, nom et password)
+    query ne retourne pas le resultat
+    On ne doit pas stocker
+
+    sprintf; des jocker sprinte
 */
-$cn = new PDO('mysql:dbname=bibliot;host=localhost', 'root','root');
+
+$dbConfig = parse_ini_file('db.ini');
+
+try{
+    $dsn = sprintf('%s:dbname=%s;host=%s', $dbConfig['driver'], $dbConfig['dbname'], $dbConfig['host']);
+
+    $cn = new PDO($dsn, $dbConfig['username'], $dbConfig['password']);
+
+    $cn->exec('SET CHARACTER SET UTF8');
+
+    $cn->exec('SET NAMES UTF8');
+}catch(PDOException $exception){
+    // redirection vers une page pour afficher une erreur relative à la connexion
+    die($exception->getMessage());
+
+}
+
+$sqlBooks = 'SELECT * FROM books';
+
+$pdoSt = $cn->query($sqlBooks);
+
+$books = $pdoSt->fetchAll();
+
+var_dump($books);
